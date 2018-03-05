c52a5e570be8bf698f0fd48b30a2198a4e67fa92
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-05 10:37:22 +0100

    Style

--- demo/src/main/java/io/spring/lab/demo/GreetingController.java
+++ demo/src/main/java/io/spring/lab/demo/GreetingController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Data;
+import lombok.Value;
 
 @RestController
 class GreetingController {
-
 
     @GetMapping("/greetings/{name}")
     Greeting greet(@PathVariable("name") String name) {
@@ -16,7 +15,7 @@ class GreetingController {
     }
 }
 
-@Data
+@Value
 class Greeting {
 
     private String message;
