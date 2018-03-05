6f75c6afbcdd0d0fb70317c89462844722419b6c
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-05 14:24:11 +0100

    Added maven config of marketing module

---
+++ marketing/src/main/java/io/spring/lab/marketing/SpringIoLabMarketingApplication.java
@@ -0,0 +1,12 @@
+package io.spring.lab.marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoLabMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoLabMarketingApplication.class, args);
+	}
+}
