d600e3310d197a488a514dbf47b4b1f07e9ed72a
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-05 09:58:48 +0100

    Added name parameter to demo

--- DemoApp.groovy
+++ DemoApp.groovy
@@ -1,8 +1,8 @@
 @RestController
 class DemoController {
 
-  @GetMapping('/greeting')
-  def greet() {
-    [message: 'Hello world']
+  @GetMapping('/greeting/{name}')
+  def greet(@PathVariable('name') String name) {
+    [message: 'Hello ' + name]
   }
 }
