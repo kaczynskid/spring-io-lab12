771de036936239ffad289a3e85d529e4d5140fdc
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-05 11:35:10 +0100

    Used TestRestTemplate instead of @LocalServerPort

--- demo/src/test/java/io/spring/lab/demo/GreetingControllerTest.java
+++ demo/src/test/java/io/spring/lab/demo/GreetingControllerTest.java
@@ -4,35 +4,26 @@ import static io.spring.lab.demo.GreetingController.GREETINGS_PATH;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class GreetingControllerTest {
 
-    @LocalServerPort int port;
-    String baseUrl;
-
-    RestTemplate rest = new RestTemplate();
-
-    @Before
-    public void setUp() throws Exception {
-        baseUrl = "http://localhost:" + port;
-    }
+    @Autowired TestRestTemplate rest;
 
     @Test
     public void shouldGreetJane() throws Exception {
 
         ResponseEntity<Greeting> response = rest.getForEntity(
-                baseUrl + GREETINGS_PATH, Greeting.class, "Jane");
+                GREETINGS_PATH, Greeting.class, "Jane");
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
