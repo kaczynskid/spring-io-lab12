e865326637cf3e2f8a3dbf4be3d688a0cc7616c2
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-05 13:21:44 +0100

    Exit with code 11 when no greeting template

--- demo/src/main/java/io/spring/lab/demo/GreetingController.java
+++ demo/src/main/java/io/spring/lab/demo/GreetingController.java
@@ -1,8 +1,12 @@
 package io.spring.lab.demo;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +21,19 @@ class GreetingController implements ApplicationRunner {
 
     public static final String GREETINGS_PATH = "/greetings/{name}";
 
-    @Value("${greeting.template:Hello %s}")
+    @Value("${greeting.template:}")
     String template;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("Defined greeting template: {}", template);
+    }
+
+    @PostConstruct
+    void requireTemplate() {
+        if (!StringUtils.hasText(template)) {
+            throw new GreetingTemplateRequired();
+        }
     }
 
     @GetMapping(GREETINGS_PATH)
@@ -43,5 +54,13 @@ class Greeting {
     @JsonCreator
     static Greeting of(@JsonProperty("message") String message) {
         return new Greeting(message);
+    }
+}
+
+class GreetingTemplateRequired extends RuntimeException implements ExitCodeGenerator {
+
+    @Override
+    public int getExitCode() {
+        return 11;
     }
 }
--- demo/src/main/java/io/spring/lab/demo/SpringIoLabDemoApplication.java
+++ demo/src/main/java/io/spring/lab/demo/SpringIoLabDemoApplication.java
@@ -2,11 +2,9 @@ package io.spring.lab.demo;
 
 import static java.util.Collections.singleton;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,7 +16,6 @@ public class SpringIoLabDemoApplication {
 		app.setSources(singleton(SpringIoLabDemoApplication.class));
 		app.setDefaultProperties(defaultProperties());
 		app.setAdditionalProfiles("profile1", "profile2");
-		app.setBannerMode(Banner.Mode.OFF);
 		app.run(args);
 	}
 
