5811bcfe429bdb9cf5ddef73a02304752cbcb6ed
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-06 10:47:19 +0100

    Added ErrorMessage

---
+++ warehouse/src/main/java/io/spring/lab/warehouse/ErrorMessage.java
@@ -0,0 +1,28 @@
+package io.spring.lab.warehouse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import lombok.Value;
+
+@Value
+public class ErrorMessage {
+
+	private String message;
+
+	public static ErrorMessage messageOf(String message) {
+		return new ErrorMessage(message);
+	}
+
+	public static ErrorMessage messageOf(Exception e) {
+		return messageOf(e.getMessage());
+	}
+
+	public static ResponseEntity<ErrorMessage> messageResponseOf(HttpStatus status, String message) {
+		return ResponseEntity.status(status).body(messageOf(message));
+	}
+
+	public static ResponseEntity<ErrorMessage> messageResponseOf(HttpStatus status, Exception e) {
+		return messageResponseOf(status, e.getMessage());
+	}
+}
