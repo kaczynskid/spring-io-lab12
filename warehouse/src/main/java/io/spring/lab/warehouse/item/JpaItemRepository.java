773979abef2c7abb3d264168ca86ba407b2d452e
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-06 15:24:35 +0100

    Added QueryDSL implementations of intem search methods

--- warehouse/src/main/java/io/spring/lab/warehouse/item/JpaItemRepository.java
+++ warehouse/src/main/java/io/spring/lab/warehouse/item/JpaItemRepository.java
@@ -1,21 +1,29 @@
 package io.spring.lab.warehouse.item;
 
-import java.util.List;
+import static io.spring.lab.warehouse.item.QItem.item;
 
-import org.springframework.data.domain.PageRequest;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.HQLTemplates;
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.AllArgsConstructor;
 
 @Repository
 @AllArgsConstructor
 public class JpaItemRepository implements ItemRepository {
 
-    interface SpringDataItemRepository extends JpaRepository<Item, Long> {
+    interface SpringDataItemRepository extends JpaRepository<Item, Long>, QueryDslPredicateExecutor<Item> {
 
         Item findTopByOrderByPriceDesc();
 
@@ -26,6 +34,9 @@ public class JpaItemRepository implements ItemRepository {
 
         @Query("from Item where name like :prefix%")
         List<Item> findByNamePrefix(@Param("prefix") String namePrefix);
+
+        @Override
+        List<Item> findAll(Predicate predicate);
     }
 
     private final SpringDataItemRepository repository;
@@ -45,14 +56,31 @@ public class JpaItemRepository implements ItemRepository {
         return repository.save(item);
     }
 
+    @PersistenceContext EntityManager jpa;
+
     @Override
     public Item findMostExpensive() {
-        return repository.findMostExpensive(new PageRequest(0, 1)).stream().findFirst()
-                .orElseThrow(() -> new RuntimeException("Empty DB!"));
+//        return repository.findTopByOrderByPriceDesc();
+
+//        return repository.findMostExpensive(new PageRequest(0, 1)).stream().findFirst()
+//                .orElseThrow(() -> new RuntimeException("Empty DB!"));
+
+        return new JPAQuery<Item>(jpa, HQLTemplates.DEFAULT)
+                .from(item)
+                .orderBy(item.price.desc())
+                .fetchFirst();
     }
 
     @Override
     public List<Item> findByNamePrefix(String prefix) {
-        return repository.findByNamePrefix(prefix);
+//        return repository.findByNameStartingWith(prefix);
+
+//        return repository.findByNamePrefix(prefix);
+
+        return repository.findAll(withNameStartingWith(prefix));
+    }
+
+    private static BooleanExpression withNameStartingWith(String prefix) {
+        return item.name.startsWith(prefix);
     }
 }
