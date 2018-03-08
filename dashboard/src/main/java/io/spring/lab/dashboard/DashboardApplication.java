5da4e22be41c844cd1b10cbcd1adf112e98b1dd1
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-08 14:16:55 +0100

    Added turbine to dashboard

--- dashboard/src/main/java/io/spring/lab/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/io/spring/lab/dashboard/DashboardApplication.java
@@ -3,9 +3,11 @@ package io.spring.lab.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class DashboardApplication {
 
 	public static void main(String[] args) {
