91c85b1c76620309c38a603ba347e5dd1296f809
Darek Kaczyński <kaczynskid@gmail.com>
2018-03-09 09:25:50 +0100

    Fixed failing tests

--- marketing/src/test/java/io/spring/lab/marketing/MarketingContractTestBase.java
+++ marketing/src/test/java/io/spring/lab/marketing/MarketingContractTestBase.java
@@ -1,17 +1,14 @@
 package io.spring.lab.marketing;
 
 import org.junit.Before;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public abstract class MarketingContractTestBase {
+public abstract class MarketingContractTestBase extends SpringTestBase {
 
     @Autowired
     WebApplicationContext applicationContext;
---
+++ marketing/src/test/java/io/spring/lab/marketing/SpringTestBase.java
@@ -0,0 +1,10 @@
+package io.spring.lab.marketing;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public abstract class SpringTestBase {
+}
